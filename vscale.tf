9e260cbdc42af2968726398d2ad0173fe3e4f2f8
Aleksei Gogolev <icetony@mail.ru>
2019-08-08 14:52:13 +0300

    [OPS-4] Add vscale.tf

---
+++ vscale.tf
@@ -0,0 +1,17 @@
+provider "vscale" {
+  token     = "my_vscale_token"
+}
+
+resource "vscale_ssh_key" "icetony" {
+  name = "icetony.ssh.key"
+  key  = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "vscale_scalet" "terr_ops4_test" {
+  count     = 1
+  name      = "ops4.test"
+  location  = "msk0"
+  make_from = "ubuntu_16.04_64_001_master"
+  rplan     = "medium"
+  ssh_keys  = ["${vscale_ssh_key.icetony.id}"]
+}
